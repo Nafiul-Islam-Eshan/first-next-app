bb5c4929d1f62dd65ab07d6d20e30dc3d2875b70
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-07-03 00:53:16 +0600

    dashboard

---
+++ src/app/dashboard/revenue/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RevenuePage = () => {
+    return (
+        <div>
+            <h1 className="text-3xl text-rose-500">Revenue Comming Soon...</h1>
+        </div>
+    );
+};
+
+export default RevenuePage;
